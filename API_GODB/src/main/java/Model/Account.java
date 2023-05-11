46f10c40da84481a76b226f1a76259f211167b30
LamNhan123 <103842795+LamNhan123@users.noreply.github.com>
2023-05-11 07:58:59 +0700

    .

--- API_GODB/src/main/java/Model/Account.java
+++ API_GODB/src/main/java/Model/Account.java
@@ -1,16 +1,36 @@
-package model;
+package Model;
 
 public class Account {
     private String accountId;
     private String accountType;
     private String customerId;
     private String staffId;
+    private boolean acc_status;
+    private String acc_password;
 
-    public Account(String accountId, String accountType, String customerId, String staffId) {
+    public boolean isAcc_status() {
+		return acc_status;
+	}
+
+	public void setAcc_status(boolean acc_status) {
+		this.acc_status = acc_status;
+	}
+
+	public String getAcc_password() {
+		return acc_password;
+	}
+
+	public void setAcc_password(String acc_password) {
+		this.acc_password = acc_password;
+	}
+
+	public Account(String accountId, String accountType, String customerId, String staffId,boolean status,String password) {
         this.accountId = accountId;
         this.accountType = accountType;
         this.customerId = customerId;
         this.staffId = staffId;
+        this.acc_status=status;
+        this.acc_password=password;
     }
 
     public String getAccountId() {
