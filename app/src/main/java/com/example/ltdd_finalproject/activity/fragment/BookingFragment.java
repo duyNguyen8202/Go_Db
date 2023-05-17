925ec7863ddaf0d03a449501bee432d86dbde739
LamNhan123 <103842795+LamNhan123@users.noreply.github.com>
2023-05-17 20:16:39 +0700

    update

---
+++ app/src/main/java/com/example/ltdd_finalproject/activity/fragment/BookingFragment.java
@@ -0,0 +1,77 @@
+package com.example.ltdd_finalproject.activity.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.ltdd_finalproject.R;
+import com.example.ltdd_finalproject.adapters.BookingAdapter;
+import com.example.ltdd_finalproject.models.Booking;
+import com.example.ltdd_finalproject.retro.API;
+import com.example.ltdd_finalproject.retro.RetrofitClient;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BookingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BookingFragment extends Fragment {
+
+    private ListView bookingListView;
+    private BookingAdapter bookingAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_booking, container, false);
+
+        bookingListView = view.findViewById(R.id.listViewBooking);
+        API api = RetrofitClient.getRetrofitLogin().create(API.class);
+
+        // Gọi phương thức getBooking() từ API
+        Call<List<Booking>> call = api.getBooking();
+        call.enqueue(new Callback<List<Booking>>() {
+            @Override
+            public void onResponse(Call<List<Booking>> call, Response<List<Booking>> response) {
+                if (response.isSuccessful()) {
+                    List<Booking> bookings = response.body();
+                    if (bookings != null) {
+                        // Cập nhật dữ liệu vào ListView
+                        bookingAdapter = new BookingAdapter(getActivity(), bookings);
+                        bookingListView.setAdapter(bookingAdapter);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Booking>> call, Throwable t) {
+                // Xử lý khi gọi API thất bại
+                if (t instanceof IOException) {
+                    // Lỗi kết nối mạng
+                    // Hiển thị thông báo lỗi cho người dùng
+                    Toast.makeText(getActivity(), "Network error. Please check your connection.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Lỗi chung
+                    // Hiển thị thông báo lỗi cho người dùng
+                    Toast.makeText(getActivity(), "An error occurred. Please try again later.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/ltdd_finalproject/retro/API.java
+++ app/src/main/java/com/example/ltdd_finalproject/retro/API.java
@@ -1,10 +1,12 @@
 package com.example.ltdd_finalproject.retro;
 import com.example.ltdd_finalproject.models.Account;
+import com.example.ltdd_finalproject.models.Booking;
 import com.example.ltdd_finalproject.models.LoginResponse;
 import com.example.ltdd_finalproject.models.ProfileResponse;
 import com.example.ltdd_finalproject.models.RegisterResponse;
 
 import java.sql.Date;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -33,5 +35,7 @@ public interface API {
     @FormUrlEncoded
     @POST("admin/account?action=profile")
     Call<ProfileResponse> getProfile (@Field("username") String username);
+    @GET("admin/booking")
+    Call<List<Booking>> getBooking();
 
 }
